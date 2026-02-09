6a2d29b9bc9ea03865b4bea6a8c7fe0e71a464cc
Jenny <205511984+jenny492@users.noreply.github.com>
2026-02-09 17:16:45 +0200

    Toteutettu sovelluksen runko

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  const handleFetch = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+      .then(response => {
+        if (!response.ok)
+          throw new Error("Error in fetch: " + response.statusText);
+
+        return response.json()
+      })
+      .then(data => setRepositories(data.items))
+      .catch(err => console.error(err));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput
+          value={keyword}
+          onChangeText={text => setKeyword(text)}
+        />
+        <Button title="Find" onPress={handleFetch} />
+        <FlatList
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
