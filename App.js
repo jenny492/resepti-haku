2b00e798f424d09e8cf944b92a94c96faab1cb51
Jenny <205511984+jenny492@users.noreply.github.com>
2026-02-09 20:15:21 +0200

    Viimeistelty toiminnallisuus ja tehty muotoilut.

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { Button, FlatList, StyleSheet, TextInput, View } from 'react-native';
+// ListSeparator https://github.com/KasperKloster/ComponentsExplained
+
 import { useState } from 'react';
+import { Button, FlatList, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
 
   const [keyword, setKeyword] = useState('');
-  const [repositories, setRepositories] = useState([]);
+  const [meals, setMeals] = useState([]);
 
   const handleFetch = () => {
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
@@ -15,20 +18,49 @@ export default function App() {
 
         return response.json()
       })
-      .then(data => setRepositories(data.items))
+      .then(data => setMeals(data.meals))
       .catch(err => console.error(err));
+  }
+
+  const listSeparator = () => {
+    return (
+      <View style={styles.separator} />
+    );
   }
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <TextInput
-          value={keyword}
-          onChangeText={text => setKeyword(text)}
-        />
-        <Button title="Find" onPress={handleFetch} />
+      <SafeAreaView style={styles.container}>
+
+        <View style={styles.input}>
+          <TextInput
+            style={styles.textInput}
+            placeholder='Keyword'
+            value={keyword}
+            onChangeText={text => setKeyword(text)}
+          />
+          <View style={styles.button}>
+            <Button title="Find" onPress={handleFetch} />
+          </View>
+        </View>
+
         <FlatList
+          style={styles.list}
+          contentContainerStyle={styles.listContent}
+          data={meals}
+          keyExtractor={(meal) => meal.idMeal}
+          ItemSeparatorComponent={listSeparator}
+          renderItem={({ item }) =>
+            <View style={styles.listItem}>
+              <Text>{item.strMeal}</Text>
+              <Image
+                style={styles.image}
+                src={item.strMealThumb}
+              />
+            </View>
+          }
         />
+
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -37,8 +69,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  textInput: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  image: {
+    width: 66,
+    height: 58,
+  },
+  input: {
+    width: '70%',
+    marginBottom: 20,
+    alignItems: 'center',
+    gap: 10,
+  },
+  button: {
+    width: 200,
+  },
+  list: {
+    paddingHorizontal: 20,
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  listContent: {
+    paddingBottom: 60,
+  },
+  listItem: {
+    margin: 20,
+  },
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#140f0f',
   },
 });
